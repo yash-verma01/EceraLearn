ab9a9f4af3f50dac02fca88efe922723919041ab
Yash Verma <yashvrm3107@gmail.com>
2025-08-30 01:18:46 +0530

    server created and datbase connected

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from 'express';
+import { connectDB } from './Model/data.js';
+
+const app = express();
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
